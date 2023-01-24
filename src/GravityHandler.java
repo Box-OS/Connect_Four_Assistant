9e8d1f07f0b283846d5b1714dac23d5e06e2301e
leqishen <leqi.shen@ashbury.ca>
2023-01-23 21:05:51 -0500

    Finished pieceCounter and rateLine methods in BoardHandler

--- src/GravityHandler.java
+++ src/GravityHandler.java
@@ -10,42 +10,6 @@ public class GravityHandler {
         return board;
     }
 
-    public static void main(String[] args) {
-        /**
-        GravityHandler g = new GravityHandler();
-        int[][] newBoard = new int[7][6];
-        newBoard = g.addPiece(newBoard, 0, 1);
-        newBoard = g.addPiece(newBoard, 0, 2);
-        newBoard = g.addPiece(newBoard, 4, 1);
-        newBoard = g.addPiece(newBoard, 4, 2);
-        newBoard = g.addPiece(newBoard, 4, 1);
-        newBoard = g.addPiece(newBoard, 4, 2);
-
-        for (int i = 0; i<7; i++) {
-            System.out.print(newBoard[i][5]);
-        }
-        System.out.println("");
-        for (int i = 0; i<7; i++) {
-            System.out.print(newBoard[i][4]);
-        }
-        System.out.println("");
-        for (int i = 0; i<7; i++) {
-            System.out.print(newBoard[i][3]);
-        }
-        System.out.println("");
-        for (int i = 0; i<7; i++) {
-            System.out.print(newBoard[i][2]);
-        }
-        System.out.println("");
-        for (int i = 0; i<7; i++) {
-            System.out.print(newBoard[i][1]);
-        }
-        System.out.println("");
-        for (int i = 0; i<7; i++) {
-            System.out.print(newBoard[i][0]);
-        }
-
-         */
-    }
+    public static void main(String[] args) {}
 
 }
