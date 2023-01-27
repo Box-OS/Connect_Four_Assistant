6fdb3bdb93188936c2c1cd200e63f0eec69f02fd
leqishen <leqi.shen@ashbury.ca>
2023-01-27 11:25:18 -0500

    added evalLine and checkForWin, which both work on the same logic. Also fixed GravityHandler adding in the wrong spots due to reversed logic.

--- src/BoardHandler.java
+++ src/BoardHandler.java
@@ -1,6 +1,89 @@
 public class BoardHandler {
 
     /**
+     * checks if there are 4 same-player pieces in a row and gives a true-or-false return
+     *
+     * @param board the game board in a 2D array
+     * @param userOrder the user the method checks for a win
+     * @return true if 4 pieces are in a row in any orientation
+     */
+    public boolean checkForWin(int[][] board, int userOrder) {
+        // check for horizontal win
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (rateLine(board[i][j], board[i][j + 1], board[i][j + 2], board[i][j + 3], userOrder) >= 1000) {
+                    return true;
+                }
+            }
+        }
+
+        // check for vertical win
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (rateLine(board[i][j], board[i + 1][j], board[i + 2][j], board[i + 3][j], userOrder) >= 1000) {
+                    return true;
+                }
+            }
+        }
+
+        // check for diagonal win (top left to bottom right)
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (rateLine(board[i][j], board[i + 1][j + 1], board[i + 2][j + 2], board[i + 3][j + 3], userOrder) >= 1000) {
+                    return true;
+                }
+            }
+        }
+
+        // check for diagonal win (top right to bottom left)
+        for (int i = 0; i < 3; i++) {
+            for (int j = 3; j < 7; j++) {
+                if (rateLine(board[i][j], board[i + 1][j - 1], board[i + 2][j - 2], board[i + 3][j - 3], userOrder) >= 1000) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * evaluates and gives a total score for the entire board situation
+     *
+     * @param board the game board in a 2D array
+     * @param userOrder userOrder the user the method checks for a win
+     * @return a total board score based on rateLine's output of each set of 4 adjacent spots
+     */
+    public int evalBoard(int[][] board, int userOrder) {
+        int score = 0;
+        // check for horizontal lines
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 4; j++) {
+                score += rateLine(board[i][j], board[i][j + 1], board[i][j + 2], board[i][j + 3], userOrder);
+            }
+        }
+        // check for vertical lines
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 7; j++) {
+                score += rateLine(board[i][j], board[i + 1][j], board[i + 2][j], board[i + 3][j], userOrder);
+            }
+        }
+        // check for diagonal lines (left to right)
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                score += rateLine(board[i][j], board[i + 1][j + 1], board[i + 2][j + 2], board[i + 3][j + 3], userOrder);
+            }
+        }
+        // check for diagonal lines (right to left)
+        for (int i = 0; i < 3; i++) {
+            for (int j = 3; j < 7; j++) {
+                score += rateLine(board[i][j], board[i + 1][j - 1], board[i + 2][j - 2], board[i + 3][j - 3], userOrder);
+            }
+        }
+        return score;
+    }
+
+    /**
      * Rates a group of 4 pieces and assigns a score based on amount of pieces in a row
      *
      * @param a first piece in row
@@ -24,7 +107,9 @@ public class BoardHandler {
         opponentCount = pieceCounter(line, opponentOrder);
 
         // check if user has winning line
-        if (userCount == 3 && opponentCount == 0) {
+        if (userCount == 4 && opponentCount == 0) {
+            lineRating += 1000;
+        } else if (userCount == 3 && opponentCount == 0) {
             lineRating += 100;
         } else if (userCount == 2 && opponentCount == 0) {
             lineRating += 10;
@@ -36,7 +121,9 @@ public class BoardHandler {
         }
 
         // check if opponent has winning line
-        if (opponentCount == 3 && userCount == 0) {
+        if (opponentCount == 4 && userCount == 0) {
+            lineRating += 1000;
+        } else if (opponentCount == 3 && userCount == 0) {
             lineRating -= 100;
         } else if (opponentCount == 2 && userCount == 0) {
             lineRating -= 10;
@@ -50,6 +137,13 @@ public class BoardHandler {
         return lineRating;
     }
 
+    /**
+     * pieceCounter counts a player's pieces in a row for any given 4 adjacent pieces.
+     *
+     * @param line an array of 4 adjacent pieces in any orientation specified by rateLine
+     * @param player indicates if method should check for player 1 or player 2's pieces
+     * @return the amount of pieces that are in a row
+     */
     public int pieceCounter(int[] line, int player) {
         int i;
         int inRow = 0;
