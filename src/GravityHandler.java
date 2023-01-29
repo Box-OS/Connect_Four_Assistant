612669654552a087fe30bdb71bf18a002ea1b591
leqishen <leqi.shen@ashbury.ca>
2023-01-29 16:18:16 -0500

    boardHandler kind of works, it is able to find the best move within depth of 1 but fails to reach further. I included implementations of minimax for later debugging.

--- src/GravityHandler.java
+++ src/GravityHandler.java
@@ -1,5 +1,9 @@
 public class GravityHandler {
 
+    public boolean isValidMove(int[][] board, int column) {
+        return board[0][column] == 0;
+    }
+
     public int[][] addPiece(int[][] board, int column, int player) {
         for (int i = 5; i>=0; i--) {
             if (board[i][column] == 0) {
